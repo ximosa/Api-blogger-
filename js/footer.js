5d8eaabdf1c9bc5863cf1826b09eecb06c473674
astrogroupsl-dev <nadeeshafernando2020@gmail.com>
2024-04-22 16:00:07 +0530

    update

--- js/footer.js
+++ js/footer.js
@@ -1,2 +1,8 @@
 document.oncontextmenu = new Function("return false")
 document.onselectstart = new Function("return false")
+
+Object.defineProperty(console, '_commandLineAPI', {
+    get: function() {
+        throw new Error('Developer tools are disabled');
+    }
+});
--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 document.oncontextmenu = new Function("return false")
 document.onselectstart = new Function("return false")
 
+Object.defineProperty(console, '_commandLineAPI', {
+    get: function() {
+        throw new Error('Developer tools are disabled');
+    }
+});
+
 fetch('README.md')
 .then(response => response.text())
 .then(text => {
