78ae97b4079313a154b185a83bce54e6c2ed6b8a
astrogroupsl-dev <nadeeshafernando2020@gmail.com>
2024-04-24 03:22:01 +0530

    updated `navbar.js` for debugging purposes.

--- js/navbar.js
+++ js/navbar.js
@@ -5,20 +5,26 @@ function deployContent(contentPath, target) {
   switch(target) {
     case '_content-iframe':
       parent.document.getElementById('content-iframe').src = contentPath;
+      alert(`File path: ${contentPath}\nTarget: ${target}`);
       break;
     case '_blank':
       window.open(contentPath, target);
+      alert(`File path: ${contentPath}\nTarget: ${target}`);
       break;
     case '_parent':
       parent.location.href = contentPath;
+      alert(`File path: ${contentPath}\nTarget: ${target}`);
       break;
     case '_self':
       window.location.href = contentPath;
+      alert(`File path: ${contentPath}\nTarget: ${target}`);
       break;
     case '_top':
       top.location.href = contentPath;
+      alert(`File path: ${contentPath}\nTarget: ${target}`);
       break;
     default:
       alert('Invalid target specified.');
+      alert(`File path: ${contentPath}\nTarget: ${target}`);
   }
 }
